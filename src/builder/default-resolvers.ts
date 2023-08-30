a6626dac98f6da3df7d22782c0d2078e43184287
nullishamy <git@amyerskine.me>
2023-08-30 20:25:17 +0100

    doc: resolvers & builtins

--- src/builder/default-resolvers.ts
+++ src/builder/default-resolvers.ts
@@ -9,9 +9,13 @@ export class EnvironmentResolver extends Resolver {
     return platform.getEnv(envKey) !== undefined
   }
 
-  resolveKey (key: string, opts: StoredParserOpts): string | undefined {
+  resolveKey (key: string, opts: StoredParserOpts): string {
     const envKey = `${opts.environmentPrefix}_${key.toUpperCase()}`
     const platform = currentPlatform()
-    return platform.getEnv(envKey)
+    const value = platform.getEnv(envKey)
+    if (value === undefined) {
+      throw new TypeError('value was undefined, but keyExists must have been true')
+    }
+    return value
   }
 }
